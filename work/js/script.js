78a24ef8e54e997e976a2c2ea3325fe46140ca0e
fanjiahui1998 <68629718+fanjiahui1998@users.noreply.github.com>
2020-10-07 14:54:34 +0800

    change2

--- work/js/script.js
+++ work/js/script.js
@@ -7,15 +7,8 @@ $(function(){
     $(".close_button").on("click",function(){
         $(this).parent(".content").slideUp();
     })
-      
+    
 });
 
-$(document).ready(function(){
-    $("button").click(function(){
-      $("#img01").fadeIn();
-      $("#img02").fadeIn("slow");
-      $("#img03").fadeIn(3000);
-    });
-  });
 
 
